aaf8ff2f3bb6fb8ea47acbd381b19139207fd69a
Matteo Comisso <teomatteo89@gmail.com>
2014-12-25 22:50:04 +0100

    Synced HomeStatusViewController and PaperDetailsViewController

--- papertron/HomeStatusViewController.swift
+++ papertron/HomeStatusViewController.swift
@@ -8,22 +8,46 @@
 
 import Foundation
 
+// Class for tableview in this viewController
+class papersCell : UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var speakerLable: UILabel!
+    @IBOutlet weak var voteCountLabel: UILabel!
+}
+
 class HomeStatusViewController : UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
+
+    //Outlets and variables
     @IBOutlet weak var tableView: UITableView!
-    
     var dataSource = [PFObject]()
     
+    //MARK:- Standard view methods
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //Some awesome stuff
         self.downloadPapers()
     }
     
+    // Downloads the papers from the Parse Backend.
     func downloadPapers() {
+        //Create a query for the Table "Papers"
         let queryPaper = PFQuery(className: "Papers")
+
+        // Performs the query online, fetching all the objects
+        let papersFound = queryPaper.findObjects() as NSArray
         
+        //Pins all the previously fetched objects inside the localDataStorage
+        PFObject.pinAllInBackground(papersFound, block: { (success: Bool, error: NSError!) -> Void in
+            if success {
+                println("Objects successfully pinned in local datastorage")
+            }
+        })
+        
+        //Points the query to localDataStorage
+        queryPaper.fromLocalDatastore()
+        
+        //Finds the objects for the tableview, looking inside the previously saved data
         queryPaper.findObjectsInBackgroundWithBlock { (response: [AnyObject]!, error: NSError!) -> Void in
             if (error == nil) {
                 if let papersObjects = response as? [PFObject] {
@@ -34,45 +58,51 @@ class HomeStatusViewController : UIViewController, UITableViewDelegate, UITableV
         }
     }
 
+}
+
+//MARK:- TABLEVIEW DELEGATE METHODS
+extension HomeStatusViewController {
+    
+    
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
+    
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.dataSource.count
     }
     
+    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell:papersCell = tableView.dequeueReusableCellWithIdentifier("cellIdentifier") as papersCell
-        
         let paperObject = self.dataSource[indexPath.row] as PFObject
-        
         cell.titleLabel.text = paperObject.objectForKey("Title") as? String
+
+        //Takes the user of the current paper and fetches its informations, updating the view
+        var speaker = paperObject.objectForKey("User") as? PFUser
+        speaker?.fetchInBackgroundWithBlock({ (userData: PFObject!, error: NSError!) -> Void in
+            if (error == nil) {
+                println(speaker?.description)
+                cell.speakerLable.text = "Speaker: ".stringByAppendingString(speaker?.username as String!)
+            }
+        })
         
         return cell
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        self.performSegueWithIdentifier("paperDetails", sender: self.dataSource[indexPath.row])
+        self.performSegueWithIdentifier("paperDetails", sender: self)
     }
 
-    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if (segue.identifier == "paperDetails") {
             let pdvc = segue.destinationViewController as PaperDetailsViewController
-            let paperObject = sender as? PFObject
-
-            pdvc.paperID = paperObject?.objectId as String!
+            let indexPath = self.tableView.indexPathForSelectedRow()
+            let paperObject = self.dataSource[indexPath?.row as Int!]
+            pdvc.paperObject = paperObject
+            pdvc.paperID = paperObject.objectId as String!
         }
     }
     
-}
-
-class papersCell : UITableViewCell {
-    
-    @IBOutlet weak var titleLabel: UILabel!
-    
-    @IBOutlet weak var speakerLable: UILabel!
-    
-    @IBOutlet weak var voteCountLabel: UILabel!
 }
--- papertron/PaperDetailsViewController.swift
+++ papertron/PaperDetailsViewController.swift
@@ -11,14 +11,54 @@ import Foundation
 class PaperDetailsViewController : UIViewController {
     
     var paperID: NSString!
+    var paperObject: PFObject!
+    var currentVote: Int?
     
     @IBOutlet weak var paperIDLabel: UILabel!
+    @IBOutlet weak var paperTitleLabel: UILabel!
+    @IBOutlet weak var speakerNameLabel: UILabel!
+    @IBOutlet weak var descriptionTextView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.paperIDLabel.text = self.paperID
+        self.paperTitleLabel.text = self.paperObject["Title"] as? String
+        self.descriptionTextView.text = self.paperObject["Description"] as? String
+//        self.speakerNameLabel.text = "Speaker: ".stringByAppendingString(self.paperObject["test"] as String)
         
+        self.loadVoteForCurrentPaper()
+    }
+    
+    func loadVoteForCurrentPaper() {
+        println(self.paperObject.description)
+        
+        let query = PFQuery(className: "PapersForPool")
+        query.whereKey("Papers", equalTo: paperObject)
+        query.limit = 1
+        query.findObjectsInBackgroundWithBlock { (response: [AnyObject]!, error: NSError!) -> Void in
+            if (error == nil) {
+                //All good
+                if let pfObjects = response as [PFObject]! {
+                    self.currentVote = pfObjects[0].objectForKey("Votes") as? Int
+                    self.paperIDLabel.text = "Votes: ".stringByAppendingFormat("%d", self.currentVote!)
+                }
+            }
+            else {
+                println("Something went wrong while finding objects \"PapersForPool\". Error: \(error.localizedDescription, error.localizedFailureReason)")
+            }
+        }
+        
+    }
+    
+    @IBAction func voteOrUnvote(sender: UIButton) {
+        if (sender.titleLabel?.text == "Vote") {
+            //Perform vote ++
+            
+        }
+        else {
+            //Perform vote --
+        }
     }
     
 }
