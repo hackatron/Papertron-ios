3ea4b1fc6c343b5f6ab2653bfd875d99f0044298
Matteo Comisso <teomatteo89@gmail.com>
2014-12-25 22:46:29 +0100

    LocalDataStorage and Analytics in AppDelegate

--- papertron/AppDelegate.swift
+++ papertron/AppDelegate.swift
@@ -27,13 +27,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             let clientKey = keysDictionary["clientKey"] as? String
             
             ParseCrashReporting.enable()
-            
+            Parse.enableLocalDatastore()
             Parse.setApplicationId(applicationId, clientKey: clientKey)
-            
+            PFAnalytics.trackAppOpenedWithLaunchOptionsInBackground(launchOptions, block: nil)
         }
 
         return true
     }
+    
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
