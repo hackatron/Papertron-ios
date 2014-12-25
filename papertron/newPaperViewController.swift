19a48d93e99195d289e9febd3c5c79fc98faaf69
Matteo Comisso <teomatteo89@gmail.com>
2014-12-25 22:48:42 +0100

    AutoLayout settings updated, added "newPaperViewController"

--- papertron/newPaperViewController.swift
+++ papertron/newPaperViewController.swift
@@ -8,22 +8,42 @@
 
 import Foundation
 
-class newPaperViewController : UIViewController {
+class newPaperViewController : UIViewController, UIAlertViewDelegate {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var additionalInfo1TextField: UITextField!
+    
+    var canBeSent: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
+
+    //Check if the paper data is acceptable
     func checkPaper() -> Bool {
         
         return true
-
     }
     
     func sendPaper() {
+        //If the paper have all the data correctly set
         if checkPaper() {
             let newPaper = PFObject(className: "Paper")
+            newPaper.setObject(PFUser.currentUser(), forKey: "User")
             
         }
     }
+    
+    @IBAction func sendToPapertron(sender: AnyObject) {
+        let confirmationCheck = UIAlertController(title: "Sending", message: "The new paper will be submitted", preferredStyle: UIAlertControllerStyle.Alert)
+        confirmationCheck.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil))
+        confirmationCheck.addAction(UIAlertAction(title: "Send", style: UIAlertActionStyle.Default, handler: { (alertAction: UIAlertAction!) -> Void in
+            println("the paper is sent!")
+            self.sendPaper()
+        }))
+    }
+    
 }
